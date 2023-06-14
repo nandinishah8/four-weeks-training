0a7130df6ef06c5d8a6e8fcdb4772fc7efe3aedc
nandinishah8 <nandinishah8501@gmail.com>
2023-06-14 17:11:51 +0530

    commit all

--- week-1/day-3/exercise-2/ShortesPathAlgorithm/Program.cs
+++ week-1/day-3/exercise-2/ShortesPathAlgorithm/Program.cs
@@ -27,15 +27,15 @@
             RunTestcase3();
             RunTestcase4();
             RunTestcase5();
-
+​
         }
-
+​
         private static void RunTestcase1()
         {
             Console.WriteLine("Testcase 1:");
             int vertices = 6;
             Graph graph = new Graph(vertices);
-
+​
             graph.AddEdge(0, 1, 4);
             graph.AddEdge(0, 2, 3);
             graph.AddEdge(1, 3, 2);
@@ -43,16 +43,16 @@
             graph.AddEdge(2, 3, 4);
             graph.AddEdge(3, 4, 2);
             graph.AddEdge(4, 5, 6);
-
+​
             int source = 0;
             int[] distances = graph.DijkstraShortestPath(source);
-
+​
             Console.WriteLine("Vertex\tDistance from Source");
             for (int i = 0; i < vertices; i++)
             {
                 Console.WriteLine($"{i}\t{distances[i]}");
             }
-
+​
             // Expected output:
             // Vertex  Distance from Source
             // 0       0
@@ -61,7 +61,7 @@
             // 3       6
             // 4       8
             // 5       14
-
+​
             Console.WriteLine();
         }
         private static void RunTestcase2()
@@ -69,7 +69,7 @@
             Console.WriteLine("Testcase 2:");
             int vertices = 5;
             Graph graph = new Graph(vertices);
-
+​
             graph.AddEdge(0, 1, 10);
             graph.AddEdge(0, 3, 5);
             graph.AddEdge(1, 2, 1);
@@ -80,16 +80,16 @@
             graph.AddEdge(3, 4, 2);
             graph.AddEdge(4, 0, 7);
             graph.AddEdge(4, 2, 6);
-
+​
             int source = 0;
             int[] distances = graph.DijkstraShortestPath(source);
-
+​
             Console.WriteLine("Vertex\tDistance from Source");
             for (int i = 0; i < vertices; i++)
             {
                 Console.WriteLine($"{i}\t{distances[i]}");
             }
-
+​
             // Expected output:
             // Vertex  Distance from Source
             // 0       0
@@ -97,7 +97,7 @@
             // 2       8
             // 3       5
             // 4       7
-
+​
             Console.WriteLine();
         }
         private static void RunTestcase3()
@@ -105,7 +105,7 @@
             Console.WriteLine("Testcase 3:");
             int vertices = 5;
             Graph graph = new Graph(vertices);
-
+​
             graph.AddEdge(0, 1, 3);
             graph.AddEdge(0, 2, 5);
             graph.AddEdge(1, 2, 2);
@@ -113,16 +113,16 @@
             graph.AddEdge(2, 3, 1);
             graph.AddEdge(2, 4, 4);
             graph.AddEdge(3, 4, 2);
-
+​
             int source = 0;
             int[] distances = graph.DijkstraShortestPath(source);
-
+​
             Console.WriteLine("Vertex\tDistance from Source");
             for (int i = 0; i < vertices; i++)
             {
                 Console.WriteLine($"{i}\t{distances[i]}");
             }
-
+​
             // Expected output:
             // Vertex  Distance from Source
             // 0       0
@@ -130,7 +130,7 @@
             // 2       5
             // 3       6
             // 4       8
-
+​
             Console.WriteLine();
         }
         private static void RunTestcase4()
@@ -138,26 +138,26 @@
             Console.WriteLine("Testcase 4:");
             int vertices = 3;
             Graph graph = new Graph(vertices);
-
+​
             graph.AddEdge(0, 1, 2);
             graph.AddEdge(1, 2, 3);
             graph.AddEdge(2, 0, 1);
-
+​
             int source = 1;
             int[] distances = graph.DijkstraShortestPath(source);
-
+​
             Console.WriteLine("Vertex\tDistance from Source");
             for (int i = 0; i < vertices; i++)
             {
                 Console.WriteLine($"{i}\t{distances[i]}");
             }
-
+​
             // Expected output:
             // Vertex  Distance from Source
             // 0       2
             // 1       0
             // 2       3
-
+​
             Console.WriteLine();
         }
         private static void RunTestcase5()
@@ -165,28 +165,28 @@
             Console.WriteLine("Testcase 5:");
             int vertices = 4;
             Graph graph = new Graph(vertices);
-
+​
             graph.AddEdge(0, 1, 1);
             graph.AddEdge(0, 3, 3);
             graph.AddEdge(1, 2, 2);
             graph.AddEdge(2, 3, 1);
-
+​
             int source = 3;
             int[] distances = graph.DijkstraShortestPath(source);
-
+​
             Console.WriteLine("Vertex\tDistance from Source");
             for (int i = 0; i < vertices; i++)
             {
                 Console.WriteLine($"{i}\t{distances[i]}");
             }
-
+​
             // Expected output:
             // Vertex  Distance from Source
             // 0       3
             // 1       3
             // 2       1
             // 3       0
-
+​
             Console.WriteLine();
         }
     }
@@ -194,71 +194,71 @@
     {
         private int vertices;
         private List<Tuple<int, int>>[] adjacencyList;
-
+​
         public Graph(int vertices)
         {
             this.vertices = vertices;
             adjacencyList = new List<Tuple<int, int>>[vertices];
-
+​
             for (int i = 0; i < vertices; i++)
             {
                 adjacencyList[i] = new List<Tuple<int, int>>();
             }
         }
-
+​
         public void AddEdge(int source, int destination, int weight)
         {
             adjacencyList[source].Add(new Tuple<int, int>(destination, weight));
             adjacencyList[destination].Add(new Tuple<int, int>(source, weight));
         }
-
+​
         public int[] DijkstraShortestPath(int source)
         {
             int[] distances = new int[vertices];
             bool[] visited = new bool[vertices];
-
+​
             for (int i = 0; i < vertices; i++)
             {
                 distances[i] = int.MaxValue;
                 visited[i] = false;
             }
-
-            distances[source] = int.MinValue;
-
+​
+            distances[source] = 0;
+​
             for (int count = 0; count < vertices - 1; count++)
             {
                 int u = MinimumDistance(distances, visited);
                 visited[u] = true;
-
+​
                 foreach (var neighbor in adjacencyList[u])
                 {
                     int v = neighbor.Item1;
                     int weight = neighbor.Item2;
-
+​
                     if (!visited[v] && distances[u] != int.MaxValue && distances[u] + weight < distances[v])
                     {
                         distances[v] = distances[u] + weight;
                     }
                 }
             }
-
+​
             return distances;
         }
-
+​
         private int MinimumDistance(int[] distances, bool[] visited)
         {
             int minDistance = int.MaxValue;
             int minIndex = -1;
-
+​
             for (int v = 0; v < vertices; v++)
             {
-                if (!visited[v] && distances[v] <= minDistance)
+                if (!visited[v] && distances[v] < minDistance)
                 {
                     minDistance = distances[v];
                     minIndex = v;
                 }
             }
-
+​
             return minIndex;
         }
     }
