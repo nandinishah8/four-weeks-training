de9a21db2dd7329f1ec66c0905c973b53858f59e
nandinishah8 <nandinishah8501@gmail.com>
2023-06-15 14:36:04 +0530

    commit all

--- week-1/day-4/exercise-2/IteratorsApp/Program.cs
+++ week-1/day-4/exercise-2/IteratorsApp/Program.cs
@@ -10,11 +10,20 @@
                 Console.WriteLine(number);
             }
         }
-        // https://www.c-sharpcorner.com/UploadFile/5ef30d/understanding-yield-return-in-C-Sharp/
-        // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/yield
+        
         public static IEnumerable<int> FibonacciSequence()
         {
-            throw new NotImplementedException();
+            int current = 0;
+            int next = 1;
+
+            while (true)
+            {
+                yield return current;   
+                int temp = current;
+                current = next;
+                next = temp + next;
+            }
+            
         }
     }
 }
