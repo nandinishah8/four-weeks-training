2456418edb4e20ae9492a20a5624adb202a64ca5
nandinishah8 <nandinishah8501@gmail.com>
2023-06-15 14:54:11 +0530

    commit all

--- week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
+++ week-1/day-4/exercise-3/LinqToObjectsApp/Program.cs
@@ -14,10 +14,47 @@
 
 
             //Write queries using LINQ for following operations
+
             //1. Get all people from USA
+            var peopleFromUSA = people.Where(p => p.Country == "USA");
+            Console.WriteLine("People from USA:");
+            foreach (var person in peopleFromUSA)
+            {
+                Console.WriteLine($"{person.Name}, {person.Age}, {person.Country}");
+            }
+            Console.WriteLine();
+
             //2. Get all people above 30
+
+            var peopleAbove30 = people.Where(p => p.Age > 30);
+            Console.WriteLine("People above 30:");
+            foreach (var person in peopleAbove30)
+            {
+                Console.WriteLine($"{person.Name}, {person.Age}, {person.Country}");
+            }
+            Console.WriteLine();
+
+
             //3. Sort people by name
+
+            var sortedPeopleByName = people.OrderBy(p => p.Name);
+            Console.WriteLine("People sorted by name:");
+            foreach (var person in sortedPeopleByName)
+            {
+                Console.WriteLine($"{person.Name}, {person.Age}, {person.Country}");
+            }
+            Console.WriteLine();
+
+
             //4. Project/Select only Name and Country of all people
+
+            var nameAndCountry = people.Select(p => new { p.Name, p.Country });
+            Console.WriteLine("Name and Country of all people:");
+            foreach (var person in nameAndCountry)
+            {
+                Console.WriteLine($"{person.Name}, {person.Country}");
+            }
+            Console.WriteLine();
 
         }
     }
