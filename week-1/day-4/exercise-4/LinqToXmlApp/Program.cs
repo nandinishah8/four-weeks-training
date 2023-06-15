59dc5fb710b5b6986a4f52009ddf8d2ea9ddcfbe
nandinishah8 <nandinishah8501@gmail.com>
2023-06-15 15:18:08 +0530

    commit all

--- week-1/day-4/exercise-4/LinqToXmlApp/Program.cs
+++ week-1/day-4/exercise-4/LinqToXmlApp/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace LinqToXmlApp
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+
+
+namespace LinqToXmlApp
 {
     internal class Program
     {
@@ -16,28 +22,50 @@
             // Write above book structure as a c# string
             string xmlString = @"<Books>
                                     <Book>
-                                        <Title>Book Title 1</Title>
-                                        <Author>Author 1</Author>
-                                        <Genre>Genre 1</Genre>
+                                        <Title>The Shining</Title>
+                                        <Author>Narayana Murthy</Author>
+                                        <Genre>Thriller</Genre>
                                     </Book>
                                     <Book>
-                                        <Title>Book Title 2</Title>
-                                        <Author>Author 2</Author>
-                                        <Genre>Genre 2</Genre>
+                                        <Title>The Hunger Games</Title>
+                                        <Author>Stephan king</Author>
+                                        <Genre>Dystopian</Genre>
                                     </Book>
                                     <Book>
-                                        <Title>Book Title 3</Title>
-                                        <Author>Author 3</Author>
-                                        <Genre>Genre 3</Genre>
+                                        <Title>The Notebook</Title>
+                                        <Author>Nicholas Spark</Author>
+                                        <Genre>Romance</Genre>
                                     </Book>
                                 </Books>";
 
             // Create an XDocument object from the XML string
 
+            XDocument xmlDoc = XDocument.Parse(xmlString);
+
+
             // Write the title of all books to the console
+           
+            var allTitles = xmlDoc.Descendants("Title").Select(t => t.Value);
+            Console.WriteLine("All Book Titles:");
+            foreach (var title in allTitles)
+            {
+                Console.WriteLine(title);
+            }
+            Console.WriteLine();
+
+
 
             // Write the title of all books with genre "Genre 1" to the console
 
+            var genre1Titles = xmlDoc.Descendants("Book")
+                .Where(b => (string)b.Element("Genre") == "Romance")
+                .Select(b => (string)b.Element("Title"));
+            Console.WriteLine("Book Titles with Romance:");
+            foreach (var title in genre1Titles)
+            {
+                Console.WriteLine(title);
+            }
+            Console.WriteLine();
         }
     }
 }
