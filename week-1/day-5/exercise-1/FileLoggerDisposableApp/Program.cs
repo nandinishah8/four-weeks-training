cf529bfe490a01f80916673a32c626c79c8b1d73
nandinishah8 <nandinishah8501@gmail.com>
2023-06-16 12:13:53 +0530

    commit all

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             // Use FileLogger and dispose of it properly
+            string filePath = "C:\\Users\\promact\\source\\repos\\Nandini\\week-1\\day-5\\exercise-1\\FileLoggerDisposableApp\\log.txt";
+
+            using (FileLogger logger = new FileLogger(filePath))
+            {
+                logger.Log("This is a log message 1");
+                logger.Log("This is a log message 2");
+                logger.Log("This is a log message 3");
+
+            }
+
+            Console.WriteLine("Logging complete. Press any key to exit.");
+            Console.ReadKey();
+
         }
     }
 
@@ -15,16 +28,30 @@
         public FileLogger(string filePath)
         {
             // Initialize StreamWriter instance
+            _writer = new StreamWriter(filePath, true);
         }
 
         public void Dispose()
         {
             // Implement IDisposable pattern
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _writer.Dispose();
+            }
+        }
+
 
         public void Log(string message)
         {
             // Write message to the file
+            _writer.WriteLine(message);
+
         }
     }
 }
